665f5275ac92bbfa25e1383beddecfb4a5db9e8e
Nguyen Xuan Hai <nguyenxuanhai2601@gmailcom>
2026-04-03 21:16:20 +0700

    update5

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-              ['html'],
+              ['html', { open: 'never' }],
               ['junit', { outputFile: 'test-results/junit.xml' }]
             ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
